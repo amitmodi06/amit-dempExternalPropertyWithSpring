d3d21b5aaec8d0b7ae9852bab5b8504671d0d39b
amitmodi06 <amitmodi06@gmail.com>
2020-04-24 18:06:59 +0530

    Using OS environment variables.

--- src/main/java/amit/springframework/amitdempExternalPropertyWithSpring/config/PropertyConfig.java
+++ src/main/java/amit/springframework/amitdempExternalPropertyWithSpring/config/PropertyConfig.java
@@ -1,11 +1,13 @@
 package amit.springframework.amitdempExternalPropertyWithSpring.config;
 
 import amit.springframework.amitdempExternalPropertyWithSpring.examplebeans.FakeDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
 
 /**
  * created by KUAM on 4/24/2020
@@ -13,6 +15,9 @@ import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 @Configuration
 @PropertySource("classpath:datasource.properties")
 public class PropertyConfig {
+
+    @Autowired
+    Environment env;
 
     @Value("${amit.username}")
     private String user;
@@ -26,7 +31,7 @@ public class PropertyConfig {
     @Bean
     public FakeDataSource fakeDataSource(){
         FakeDataSource fakeDataSource = new FakeDataSource();
-        fakeDataSource.setNameUser(user);
+        fakeDataSource.setNameUser(env.getProperty("USERNAME"));
         fakeDataSource.setPassword(password);
         fakeDataSource.setUrldb(url);
         return fakeDataSource;
